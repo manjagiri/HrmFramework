7e4f656c4632c66e65179ca87298ff88611cd57c
BHOOMIKA <BHOOMIKA@lakshmisagar>
2022-12-16 12:38:39 +0530

    added sop statement with my name

--- src/HRMM/HRMN.java
+++ src/HRMM/HRMN.java
@@ -5,6 +5,7 @@ public class HRMN {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(" iam wrote one programming");
-	}
+		System.out.println("sagar");
+		}
 
 }
