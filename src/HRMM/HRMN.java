367a37f5b28053124274374141d8f5fa676fdd30
BHOOMIKA <BHOOMIKA@lakshmisagar>
2022-12-16 12:34:23 +0530

    i am added one more programming

---
+++ src/HRMM/HRMN.java
@@ -0,0 +1,10 @@
+package HRMM;
+
+public class HRMN {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" iam wrote one programming");
+	}
+
+}
